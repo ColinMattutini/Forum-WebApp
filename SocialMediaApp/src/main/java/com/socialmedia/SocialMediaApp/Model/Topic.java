c8c727457e0a087d610b95d181e7fd3e6034e308
Colin <cmmatt14@gmail.com>
2022-10-28 16:52:39 -0400

    topic entity

---
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/Topic.java
@@ -0,0 +1,17 @@
+package com.socialmedia.SocialMediaApp.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topic {
+    //private Long topicId;
+    //private String topicName;
+    //private String topicDescription
+}
