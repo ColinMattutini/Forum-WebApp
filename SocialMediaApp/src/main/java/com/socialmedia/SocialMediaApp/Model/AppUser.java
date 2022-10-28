acdcc10a886362e2964a96f937ff698fa7b3b233
Colin <cmmatt14@gmail.com>
2022-10-28 16:53:45 -0400

    user entity

---
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/AppUser.java
@@ -0,0 +1,22 @@
+package com.socialmedia.SocialMediaApp.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+
+    //private Long appUserId
+    //private String firstName
+    //private String lastName
+    //private String email
+    //private String password
+    //private String username
+
+}
