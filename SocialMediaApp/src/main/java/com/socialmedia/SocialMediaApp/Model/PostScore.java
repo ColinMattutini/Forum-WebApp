a2110fff91ae0dd29f9238a4cd064b66b1094c70
Colin <cmmatt14@gmail.com>
2022-10-28 16:53:20 -0400

    post score entity

---
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/PostScore.java
@@ -0,0 +1,11 @@
+package com.socialmedia.SocialMediaApp.Model;
+
+public class PostScore {
+
+    //private Long userReview Id
+    //private ENUM review
+
+    //One User many reviews (only one per post)
+    //private AppUser appUser
+    //private Post post;
+}
