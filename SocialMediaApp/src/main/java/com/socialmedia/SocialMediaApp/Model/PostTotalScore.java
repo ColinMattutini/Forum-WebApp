fc89f2e4cc024991fabeb1cb8f6fe4d91948290c
Colin <cmmatt14@gmail.com>
2022-10-28 16:53:11 -0400

    post total score entity

---
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/PostTotalScore.java
@@ -0,0 +1,11 @@
+package com.socialmedia.SocialMediaApp.Model;
+
+public class PostTotalScore {
+
+    //private Long postScoreId
+    //private Integer positiveScore
+    //private Integer negative Score
+
+    //One Score to One Post One to one
+    //private Post post;
+}
