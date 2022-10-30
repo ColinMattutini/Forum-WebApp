45f9cb7dce0d5ebfea632056d6458150037610c4
Colin <cmmatt14@gmail.com>
2022-10-30 15:39:16 -0400

    appUser repo and service updated with methods

--- SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Repo/AppUserRepo.java
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Repo/AppUserRepo.java
@@ -3,7 +3,11 @@ package com.socialmedia.SocialMediaApp.Repo;
 import com.socialmedia.SocialMediaApp.Model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepo extends JpaRepository<AppUser, Long> {
 
     AppUser findByEmail(String email);
+    AppUser findByUsername(String username);
+    //Optional<AppUser> findByEmail(String email);
 }
--- SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Service/AppUserService.java
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Service/AppUserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 public interface AppUserService {
 
-    void saveAppUser(AppUser appUser);
+    AppUser saveAppUser(AppUser appUser);
     AppUser getAppUser(String email);
     void saveRole(Role role);
     void addRoleToAppUser(String email, String role);
