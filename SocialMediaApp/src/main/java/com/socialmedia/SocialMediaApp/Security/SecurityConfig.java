7e71bbe3f37273da69663f4ff774d0cd789674a7
Colin <cmmatt14@gmail.com>
2022-10-30 22:09:43 -0400

    enable login and temp permit all requests

---
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.socialmedia.SocialMediaApp.Security;
+
+import com.socialmedia.SocialMediaApp.Util.CustomAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration @EnableWebSecurity @RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AuthenticationConfiguration configuration;
+
+    @Bean
+    @CrossOrigin(origins = "http://localhost:3000")
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        CustomAuthFilter customAuthenticationFilter = new CustomAuthFilter(authenticationManager());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.cors().and().csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        //http.addFilterBefore(new CustomAuthFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilter(customAuthenticationFilter);
+        //http.authorizeRequests().anyRequest();
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Autowired
+    void configure(AuthenticationManagerBuilder builder) throws Exception{
+        builder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Control-Type", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
