0b717c3ef565ac61222092e5c8697d12e86aaa26
Colin <cmmatt14@gmail.com>
2022-10-30 21:18:18 -0400

    entity updated with relationship mapping

--- SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/PostComment.java
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/PostComment.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
@@ -12,14 +13,20 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 public class PostComment {
 
-    //private Long commentId;
-    //private String/Text? comment;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
 
-    //private Many comments to one post
-    //private Post post;
-    //private Date commentCreationDate;
+    private String comment;
 
-    //Many commments to one user
-    //private AppUser appUser;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    private Date commentCreationDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "appUserId", referencedColumnName = "appUserId")
+    private AppUser appUser;
 
 }
