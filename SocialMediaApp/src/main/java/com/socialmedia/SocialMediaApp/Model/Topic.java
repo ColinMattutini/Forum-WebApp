6ccaf98aa679ae053e42ba074263de79954b9d2a
Colin <cmmatt14@gmail.com>
2022-10-30 21:18:36 -0400

    entity updated with relationship mapping

--- SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/Topic.java
+++ SocialMediaApp/src/main/java/com/socialmedia/SocialMediaApp/Model/Topic.java
@@ -5,13 +5,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Topic {
-    //private Long topicId;
-    //private String topicName;
-    //private String topicDescription
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long topicId;
+    private String topicName;
+    private String topicDescription;
 }
